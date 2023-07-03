894e8717e5a92ab00c6ec6554d3b959d12c6def8
geekgary-x <18017158729@163.com>
2023-07-03 18:26:52 +0800

    ✨ feat: powerBasisFitting

--- Apps/Viewer/Widgets/hw01.cpp
+++ Apps/Viewer/Widgets/hw01.cpp
@@ -1,5 +1,5 @@
 #include "HW01.h"
-
+#include <iostream>
 HW01::HW01(QWidget* parent)
     : QCustomPlot(parent)
 {
@@ -26,17 +26,50 @@ HW01::~HW01() {}
 void HW01::mousePressEvent(QMouseEvent* event)
 {
     if (event->button() == Qt::LeftButton) {
-        // 获取鼠标点击位置的坐标
-        double x     = xAxis->pixelToCoord(event->pos().x());
-        double y     = yAxis->pixelToCoord(event->pos().y());
-        auto   point = this->addGraph();
+        double x = xAxis->pixelToCoord(event->pos().x());
+        double y = yAxis->pixelToCoord(event->pos().y());
+        m_xValues.push_back(x);
+        m_yValues.push_back(y);
+        auto point = this->addGraph();
         point->addData(x, y);
         point->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, Qt::red, Qt::white, 8));
-        // 在这里处理选择点的逻辑
-        // 您可以在这里添加代码来记录或使用选定的点坐标
         this->replot();
+        powerBasisFitting(m_yValues, m_yValues, 10);
+    }
+    QCustomPlot::mousePressEvent(event);
+}
+
+Eigen::VectorXd HW01::powerBasisFitting(const std::vector<float>& xValues,
+                                        const std::vector<float>& yValues, int degree)
+{
+    int numPoints = xValues.size();
+
+    if (numPoints < degree + 1) {
+        std::cout << "Error: Insufficient number of points for fitting." << std::endl;
+        return Eigen::VectorXd();
     }
 
-    // 将鼠标事件传递给父类处理
-    QCustomPlot::mousePressEvent(event);
+    // 构建矩阵A和向量b
+    Eigen::MatrixXd A(numPoints, degree + 1);
+    Eigen::VectorXd b(numPoints);
+
+    for (int i = 0; i < numPoints; i++) {
+        double x = xValues[i];
+        b(i)     = yValues[i];
+
+        for (int j = 0; j <= degree; j++) {
+            A(i, j) = std::pow(x, j);
+        }
+    }
+
+    // 使用最小二乘法求解系数向量c
+    Eigen::VectorXd c = A.fullPivLu().solve(b);
+
+    // 输出拟合结果
+    std::cout << "Fitted coefficients (from c0 to cn): ";
+    for (int i = 0; i <= degree; i++) {
+        std::cout << c(i) << " ";
+    }
+    std::cout << std::endl;
+    return c;
 }
--- Apps/Viewer/Widgets/hw01.h
+++ Apps/Viewer/Widgets/hw01.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "qcustomplot.h"
+#include <Eigen/Dense>
 #include <QWidget>
+#include <vector>
+
 class HW01 : public QCustomPlot
 {
     Q_OBJECT
@@ -12,4 +15,8 @@ protected:
     void mousePressEvent(QMouseEvent* event) override;
 
 private:
+    Eigen::VectorXd    powerBasisFitting(const std::vector<float>& xValues,
+                                         const std::vector<float>& yValues, int degree);
+    std::vector<float> m_xValues;
+    std::vector<float> m_yValues;
 };
